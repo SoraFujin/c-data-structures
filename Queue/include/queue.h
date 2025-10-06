55afbb4313004b9ec27dcce615e86588ff8d1fba
SoraFujin <ahmaddiab29072002@gmail.com>
2025-10-07 02:52:00 +0300

    Fix Header

--- Queue/include/queue.h
+++ Queue/include/queue.h
@@ -5,21 +5,14 @@
 #include <stdlib.h>
 
 typedef void* QueueValue;
+typedef struct Queue Queue;
 
-typedef struct {
-    QueueValue *data; // Double void pointer for data inside the Queue
-    int head;         // Track the first element
-    int tail;         // track the last element
-    int size;         // Size of the queue
-    int capacity;     // Maximum element inside the queue
-} Queue;
-
-void print_queue(Queue *queue);
 Queue* create_queue(size_t queue_length);
 int isEmpty(Queue *queue);
 void enqueue(Queue *queue, void* data); 
 void* dequeue(Queue *queue);
 void* peek(Queue *queue);
+void print_queue(Queue *queue);
 void print_queue_info(Queue *queue);
 
 #endif
